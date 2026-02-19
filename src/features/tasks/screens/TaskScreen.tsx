a97214ae4f9d3fb659285a8c6a48c43778eda31f
VeligorMan <spaunroom@gmail.com>
2026-02-19 21:12:59 +0300

    feat(tasks): add task creation modal with text input

--- src/features/tasks/screens/TaskScreen.tsx
+++ src/features/tasks/screens/TaskScreen.tsx
@@ -1,13 +1,34 @@
-import { View, Text, Button, FlatList, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  FlatList,
+  Pressable,
+  Modal,
+  TextInput,
+} from "react-native";
+import { useState } from "react";
 import { useTasksStore } from "../../../store/tasksStore";
 
 export const TasksScreen = () => {
   const { tasks, addTask, toggleTask, removeTask } = useTasksStore();
 
+  const [modalVisible, setModalVisible] = useState(false);
+  const [title, setTitle] = useState("");
+
+  const handleAddTask = () => {
+    if (!title.trim()) return;
+
+    addTask(title);
+    setTitle("");
+    setModalVisible(false);
+  };
   return (
     <View style={{ flex: 1, padding: 16 }}>
-      <Button title="Добавить задачу" onPress={() => addTask("Новая задача")} />
+      {/* Кнопка открытия */}
+      <Button title="Добавить задачу" onPress={() => setModalVisible(true)} />
 
+      {/* Список */}
       <FlatList
         data={tasks}
         keyExtractor={(item) => item.id}
@@ -33,6 +54,35 @@ export const TasksScreen = () => {
           </Pressable>
         )}
       />
+
+      {/* Modal создания */}
+      <Modal visible={modalVisible} animationType="slide">
+        <View
+          style={{
+            flex: 1,
+            padding: 16,
+            justifyContent: "center",
+          }}
+        >
+          <Text style={{ fontSize: 20, marginBottom: 12 }}>Новая задача</Text>
+
+          <TextInput
+            placeholder="Введите текст задачи"
+            value={title}
+            onChangeText={setTitle}
+            style={{
+              borderWidth: 1,
+              borderColor: "#ccc",
+              padding: 12,
+              borderRadius: 8,
+              marginBottom: 12,
+            }}
+          />
+
+          <Button title="Сохранить" onPress={handleAddTask} />
+          <Button title="Отмена" onPress={() => setModalVisible(false)} />
+        </View>
+      </Modal>
     </View>
   );
 };
